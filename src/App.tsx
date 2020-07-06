0048b6b307d62c8818f9a309ef66b51304da6517
wyswwill <wyswill4290>
2020-07-06 15:46:52 +0800

    基本页面滑动

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,47 @@ import swiper from "swiper";
 import 'swiper/swiper-bundle.min.css'
 import MainPage from "./views/mainPage/mainPage";
 
-/*const _swiper =*/
 type Tstate = {
-  pages: string[]
+  pages: string[],
+  activeIndex: number,
+  swiper: swiper | object,
 }
 
-interface App {
-  state: Tstate
-}
-
-class App extends React.Component {
-
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      pages: ['mainPage', 'mainPage', 'mainPage', 'mainPage', 'mainPage',]
-    }
+class App extends React.Component<any, any, Tstate> {
+  public readonly state: Readonly<Tstate> = {
+    pages: ['mainPage', 'mainPage', 'mainPage', 'mainPage', 'mainPage',],
+    activeIndex: 0,
+    swiper: {},
   }
 
   componentDidMount() {
-    new swiper('.swiper-container', {
-      direction: "horizontal", // 垂直切换选项
-    })
+    const _swiper = new swiper('.swiper-container', {
+      direction: "horizontal",
+      on: {
+        slideChangeTransitionStart: ({activeIndex}) => {
+          this.setState({activeIndex})
+        },
+      }
+    });
+    this.setState({
+      swiper: _swiper
+    });
+  }
+
+  changePage(index: number) {
+    // @ts-ignore
+    this.state.swiper.slideTo(index);
   }
 
   render() {
-
     return (<div className="App">
       <div className="swiper-container">
         <div className="swiper-wrapper">
-          {
-            this.state.pages.map((ele: any, index: number) => (<div className='swiper-slide' key={index}>{<MainPage/>}</div>))}
+          {this.state.pages.map((ele: any, index: number) => (<div className='swiper-slide' key={index}>{<MainPage/>}</div>))}
         </div>
       </div>
       <div className='buttonBar'>
-        {this.state.pages.map((ele: string, index: number) => (<div className="buttonBarItem" key={index}>{ele}</div>))}
+        {this.state.pages.map((ele: string, index: number) => (<div onClick={this.changePage.bind(this, index)} className={`buttonBarItem ${this.state.activeIndex === index ? 'active' : ''}`} key={index}>{ele}</div>))}
       </div>
     </div>);
   }
