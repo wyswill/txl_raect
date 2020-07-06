6b5ed63c1b643ec11d6b7d49ab537fa433dc8beb
wyswwill <wyswill4290>
2020-07-06 14:00:07 +0800

    s

---
+++ src/types/base.d.ts
@@ -0,0 +1,26 @@
+declare interface IUser {
+  baseInfo: {
+    name: string;
+    headIcon: string;
+    title: string;
+  };
+  extInfo: {};
+}
+
+declare interface IPost {
+  title: string;
+  content?: string;
+  tag?: string;
+  headIcon?: string;
+  poster: IUser;
+  postTime: number | string;
+  options: "like" | "start" | "delete" | "comment";
+  extData?: any;
+}
+
+declare interface IMsg {
+  title: string;
+  user: IUser;
+  time: string | number;
+  content: string;
+}
